108ef52b7fe4640efaf293c10e15febd95c6fce8
Asianadian <danielchen.mc@gmail.com>
2023-03-04 02:59:45 -0500

    set up

---
+++ src/appSettings.js
@@ -0,0 +1,11 @@
+const settings = {
+    'clientId': 'acf9b3d3-0034-4ec0-8291-4f04215da0ce',
+    'tenantId': 'common',
+    'graphUserScopes': [
+      'user.read',
+      'mail.read',
+      'mail.send'
+    ]
+  };
+  
+  module.exports = settings;
